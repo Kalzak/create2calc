493b4a4fe9b43fd2dba318d9449f1eb541dea00c
Kalzak <kalzak.dev@protonmail.com>
2023-03-03 14:16:11 +0800

    added enum arguments

--- src/lib.rs
+++ src/lib.rs
@@ -3,38 +3,44 @@ use std::str::FromStr;
 
 mod create2;
 
-pub fn calc_create2_address_eth_types(
-    sender: H160,
-    salt: H256,
-    code: Vec<u8>,
-) -> Vec<u8> {
-    create2::calc_create2_address(
-        sender,
-        salt,
-        code,
-    )
+pub enum Sender {
+    Str(String),
+    H160(H160),
+    Bytes(Vec<u8>)
 }
 
-pub fn calc_create2_address_byte_vec(
-    sender: Vec<u8>,
-    salt: Vec<u8>,
-    code: Vec<u8>,
-) -> Vec<u8> {
-    create2::calc_create2_address(
-        H160::from_slice(sender.as_slice()),
-        H256::from_slice(salt.as_slice()),
-        code,
-    )
+pub enum Salt {
+    Str(String),
+    H256(H256),
+    Bytes(Vec<u8>)
 }
 
-pub fn calc_create2_address_string(
-    sender: &str,
-    salt: &str,
-    code: &str,
+pub enum Code {
+    Str(String),
+    Bytes(Vec<u8>)
+}
+
+pub fn calc_create2_address(
+    sender: &Sender,
+    salt: &Salt,
+    code: &Code
 ) -> Vec<u8> {
-    create2::calc_create2_address(
-        H160::from_str(sender).expect("Invalid address string"),
-        H256::from_str(salt).expect("Invalid salt string"),
-        hex::decode(&code[2..]).expect("Invalid code"),
-    )
+    let sender_formatted: H160 = match sender {
+        Sender::Str(s) => H160::from_str(s).expect("Invalid address string"),
+        Sender::H160(s) => *s,
+        Sender::Bytes(s) => H160::from_slice(s.as_slice())
+    };
+
+    let salt_formatted: H256 = match salt {
+        Salt::Str(s) => H256::from_str(s).expect("Invalid salt string"),
+        Salt::H256(s) => *s,
+        Salt::Bytes(s) => H256::from_slice(s.as_slice())
+    };
+
+    let code_formatted: Vec<u8> = match code {
+        Code::Str(c) => hex::decode(&c[2..]).expect("Invalid code"),
+        Code::Bytes(c) => c.clone()
+    };
+
+    create2::calc_create2_address(sender_formatted, salt_formatted, code_formatted)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,39 @@
 use ethereum_types::{H160, H256};
 use hex;
+use create2calc::{Sender, Salt, Code};
+
 
 fn main() {
-    let sender_string = "0xd9145CCE52D386f254917e481eB44e9943F39138";
-    let salt_string = "0x800e2ebd330b3c3a1b15462bc4b4f4f87c43f4e4ad30f76459c88ab9d3af3ce3";
-    let initcode_string = "0x600e600c6000396000f300600036600073";
+    /*
+    let sender_string = Sender::Str("0xd9145CCE52D386f254917e481eB44e9943F39138".to_string());
+    let salt_string = Salt::Str("0x800e2ebd330b3c3a1b15462bc4b4f4f87c43f4e4ad30f76459c88ab9d3af3ce3".to_string());
+    let code_string = Code::Str("0x600e600c6000396000f300600036600073".to_string());
 
-    let sender_ethtype = H160::from_slice(&hex::decode(&sender_string[2..]).unwrap());
-    let salt_ethtype = H256::from_slice(&hex::decode(&salt_string[2..]).unwrap());
+    let sender_ethtype = Sender::H160(H160::from_slice(&hex::decode(&"0xd9145CCE52D386f254917e481eB44e9943F39138"[2..]).unwrap()));
+    let salt_ethtype = Salt::H256(H256::from_slice(&hex::decode(&"0x800e2ebd330b3c3a1b15462bc4b4f4f87c43f4e4ad30f76459c88ab9d3af3ce3"[2..]).unwrap()));
 
-    let sender_vec = hex::decode(&sender_string[2..]).unwrap();
-    let salt_vec = hex::decode(&salt_string[2..]).unwrap();
-    let initcode_vec = hex::decode(&initcode_string[2..]).unwrap();
+    let sender_vec = Sender::Bytes(hex::decode(&"0xd9145CCE52D386f254917e481eB44e9943F39138"[2..]).unwrap());
+    let salt_vec = Salt::Bytes(hex::decode(&"0x800e2ebd330b3c3a1b15462bc4b4f4f87c43f4e4ad30f76459c88ab9d3af3ce3"[2..]).unwrap());
+    let code_vec = Code::Bytes(hex::decode(&"0x600e600c6000396000f300600036600073"[2..]).unwrap());
 
-    let mut deployed_address = create2calc::calc_create2_address_eth_types(sender_ethtype, salt_ethtype, initcode_vec.clone());
-
-    println!("0x{}", hex::encode(deployed_address));
-
-    deployed_address = create2calc::calc_create2_address_byte_vec(sender_vec, salt_vec, initcode_vec);
+    let mut deployed_address = create2calc::calc_create2_address(&sender_ethtype, &salt_ethtype, &code_vec);
 
     println!("0x{}", hex::encode(deployed_address));
 
-    deployed_address = create2calc::calc_create2_address_string(sender_string, salt_string, initcode_string);
+    deployed_address = create2calc::calc_create2_address(&sender_vec, &salt_vec, &code_vec);
 
     println!("0x{}", hex::encode(deployed_address));
 
+    deployed_address = create2calc::calc_create2_address(&sender_string, &salt_string, &code_string);
+
+    println!("0x{}", hex::encode(deployed_address));
+    */
+
+    let sender = Sender::Str("0xf8e81D47203A594245E36C48e151709F0C19fBe8".to_string());
+    let salt = Salt::Str("0x800e2ebd330b3c3a1b15462bc4b4f4f87c43f4e4ad30f76459c88ab9d3af3ce3".to_string());
+    let code = Code::Str("0x600b8060093d393df360026003015952596000f3".to_string());
+
+    let deployed_address = create2calc::calc_create2_address(&sender, &salt, &code);
+
+    println!("0x{}", hex::encode(deployed_address));
 }
